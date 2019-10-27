200cc6b45acaa812e4e545fdba217bf4b3720c4c
Geoffrey Moss <geoffmoss0@gmail.com>
2019-10-27 02:17:34 -0400

    First attempt at trie, not done yet

---
+++ commands.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+
+bool run_func(char *input) {
+	
+}
+
+trie_node search(char *input, trie_node root) {
+	if (
+}
+
+bool contains(trie_node check) {
+	
+	trie_node *list[27] = check->children;
+	for (int i = 0; i < 26; i++) {
+		if (list[i] != NULL)
+	}
+}
+
+
+///This will happen exactly once and fully create the trie
+///No modification of the trie should occur, nor should another trie be created
+///This will be hardcoded because there will be no reason for the commands to change
+trie_node create_trie() {
+
+}
